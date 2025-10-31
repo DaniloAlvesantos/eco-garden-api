698a03d0c9cb8a6c2952be2017e1ffe725438ae7
DaniloAlvesantos <daniloasan.itapira@gmail.com>
2025-10-31 14:50:57 -0300

    fix: config folder

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -1,12 +1,26 @@
-import * as fs from "node:fs";
 import admin from "firebase-admin";
+import * as fs from "node:fs";
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync("../../config/firebase.json", "utf-8")
-);
+if (!admin.apps.length) {
+  const serviceAccount = {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  };
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  });
+}
+
+if (process.env.NODE_ENV === "development") {
+  const serviceAccount = JSON.parse(
+    fs.readFileSync("../../config/firebase.json", "utf-8")
+  );
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 export const firestore_db = admin.firestore();
